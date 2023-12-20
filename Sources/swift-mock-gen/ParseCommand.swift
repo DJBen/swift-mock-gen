89081d09ed274a8e7ed741af06243e4f08f640c6
Sihao Lu <lsh32768@gmail.com>
2023-12-19 18:55:07 -0800

    Add support to read from directory and write to files

--- Sources/swift-mock-gen/ParseCommand.swift
+++ Sources/swift-mock-gen/ParseCommand.swift
@@ -11,13 +11,19 @@
 //===----------------------------------------------------------------------===//
 
 import ArgumentParser
+import Foundation
 
 struct ParseArguments: ParsableArguments {
-  @Argument(help: "The source file that should be parsed; if omitted, use stdin")
-  var sourceFile: String?
+    @Argument(
+        help: "The source files and/or directories that should be parsed; if omitted, use stdin"
+    )
+    var sourcePaths: [String]
 
-  @Option(name: [.long, .short], help: "If passed, parse this source text instead of reading source file")
-  var source: String?
+    @Option(name: [.long], help: "If passed, parse this source text instead of reading source file")
+    var source: String?
+
+    @Option(name: [.long, .short], help: "Writes generated mocks to the output directory, if provided.")
+    var outputDir: String?
 }
 
 /// A command  that has arguments to parse source code
@@ -26,22 +32,59 @@ protocol ParseCommand {
 }
 
 extension ParseCommand {
-  /// The name of the source file that can can be displayed to users
-  ///
-  /// This is not necessarily a file path if the file contents are read from
-  /// stdin or the command line arguments.
-  var sourceFileName: String {
-    return arguments.sourceFile ?? "stdin"
-  }
-
-  /// The contents of the source that should be parsed in UTF-8 bytes.
-  var sourceFileContents: [UInt8] {
-    get throws {
-      if let source = arguments.source {
-        return Array(source.utf8)
-      }
-
-      return try getContentsOfSourceFile(at: arguments.sourceFile)
+    /// The contents of the source files that should be parsed, each in UTF-8 bytes.
+    func sourceFiles() -> any IteratorProtocol<File> {
+        if let source = arguments.source {
+            return SourceFileContentIterator(fileNames: [source], directories: [])
+        } else if arguments.sourcePaths.isEmpty {
+            return StdinIterator()
+        } else {
+            return SourceFileContentIterator(sourcePaths: arguments.sourcePaths)
+        }
     }
-  }
+    
+    /// Writes a `TextOutputStreamable` content to the designated output.
+    ///
+    /// This method reads from arguments and checks `outputDir` property. If exists, it will output
+    /// to the directory as files. Otherwise it outputs to stdout.
+    /// - Parameters:
+    ///   - output: The content of the output.
+    ///   - fileName: The file name. If missing, it assumes "Mock.swift".
+    func write(_ content: any TextOutputStreamable, fromSourceFile fileName: String?) throws {
+        if let outputDir = arguments.outputDir {
+            // Ensure the output directory exists
+            let outputDirURL = URL(fileURLWithPath: outputDir)
+            if !FileManager.default.fileExists(atPath: outputDirURL.path) {
+                try FileManager.default.createDirectory(at: outputDirURL, withIntermediateDirectories: true, attributes: nil)
+            }
+            
+            let outputFileName: String
+            if let fileName = fileName {
+                // Extract the file name from the path
+                let url = URL(fileURLWithPath: fileName)
+                let fileBaseName = url.deletingPathExtension().lastPathComponent
+                let fileExtension = url.pathExtension
+
+                // Append -Mock to the file name
+                outputFileName = "\(fileBaseName)Mock.\(fileExtension)"
+            } else {
+                // Default file name with -Mock appended
+                outputFileName = "Mock.swift"
+            }
+
+            let outputUrl = URL(fileURLWithPath: outputDir).appendingPathComponent(outputFileName)
+
+            if !FileManager.default.fileExists(atPath: outputUrl.path) {
+                FileManager.default.createFile(atPath: outputUrl.path, contents: nil, attributes: nil)
+            }
+
+            let fileHandle = try FileHandle(forWritingTo: outputUrl)
+            var stream = FileHandlerOutputStream(fileHandle)
+            content.write(to: &stream)
+
+            fileHandle.closeFile()
+        } else {
+            print(content)
+        }
+    }
 }
