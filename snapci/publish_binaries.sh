cc6c2895b4ba2f40c860447196eefdf70b1c1765
Sihao Lu <slu2@snapchat.com>
2024-01-04 15:50:52 -0800

    Post PR comment about binary build completion

--- snapci/publish_binaries.sh
+++ snapci/publish_binaries.sh
@@ -2,12 +2,14 @@ set -xeuo pipefail
 
 export CI=true
 
+source $(dirname $BASH_SOURCE)/utils.sh
+
 echo "Selecting Xcode 15.0"
 sudo xcode-select -s /Applications/Xcode15.0_15A240d.app/Contents/Developer
 echo "Building and testing swift_mock_gen fat binaries..."
 
-swift build -c release --arch x86_64
-swift build -c release --arch arm64
+swift build --product swift-mock-gen -c release --arch x86_64
+swift build --product swift-mock-gen -c release --arch arm64
 
 echo "Zipping swift-mock-gen binaries"
 mkdir -p .build/dist
@@ -20,8 +22,12 @@ gsutil cp .build/dist/swift-mock-gen.zip gs://phantom-dependency-uploads/swift-m
 
 echo "Cleaning up binaries"
 rm -rf .build/dist
-echo "Done! Go to https://snapengine-builder.sc-corp.net/jenkins/job/phantom-dependency-uploader/build?delay=0sec to trigger phantom dependency uploader."
-echo "dependency_name: swift-mock-gen"
-echo "dependency_package_url: gs://phantom-dependency-uploads/swift-mock-gen.zip"
-echo "dependency_files: swift-mock-gen_arm64-apple-macosx,swift-mock-gen_x86_64-apple-macosx"
-echo "dependency_owner: APPINS"
+
+PHANTOM_DEP_UPLOADER_URL="https://snapengine-builder.sc-corp.net/jenkins/job/phantom-dependency-uploader/build?delay=0sec"
+GS_URL="gs://phantom-dependency-uploads/swift-mock-gen.zip"
+echo "Posting PR Comment..."
+escaped_url=$(escape_slashes "${PHANTOM_DEP_UPLOADER_URL}")
+escaped_gs_url=$(escape_slashes "${GS_URL}")
+comment="Done! Go to ${escaped_url} to trigger phantom dependency uploader.\r\n dependency_name: swift-mock-gen\r\n dependency_package_url: ${escaped_gs_url}\r\n dependency_files: swift-mock-gen_arm64-apple-macosx,swift-mock-gen_x86_64-apple-macosx\r\n dependency_owner: APPINS"
+
+post_comment "${comment}"
